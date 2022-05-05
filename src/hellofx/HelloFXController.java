662aa581dcd63431150ce77a3783f7f27380a8be
Md. Anisur Rahman <mrahman191191@bscse.uiu.ac.bd>
2022-05-05 09:16:06 +0600

    Log in and registration done successfully

--- src/hellofx/HelloFXController.java
+++ src/hellofx/HelloFXController.java
@@ -17,7 +17,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import hellofx.ConnectionClass;
+import java.sql.*;
+import java.util.Optional;
 
 /**
  * FXML Controller class
@@ -32,15 +37,47 @@ public class HelloFXController implements Initializable {
     @FXML private TextField phnnum;
     @FXML private TextField address;
     @FXML private TextField password;
+    @FXML private TextField gender;
+
     
     public void changeScreenButtonPushed(ActionEvent event) throws IOException
     {
-         Parent loginView = FXMLLoader.load(getClass().getResource("Log_in_page.fxml"));
-         Scene loginScene = new Scene (loginView);
-         
-         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-         window.setScene(loginScene);
-         window.show();
+        
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection = connectionClass.getConnection();
+
+        try {
+
+            Statement statement = connection.createStatement();
+
+            PreparedStatement stmt  =connection.prepareStatement("insert into user values (?,?,?,?,?,?)");
+            stmt.setString(1,name.getText());
+            stmt.setString(2,password.getText());
+            stmt.setString(3,phnnum.getText());
+            stmt.setString(4,address.getText());
+            stmt.setString(5,gender.getText());
+            stmt.setString(6,email.getText());
+            
+
+          int status = stmt.executeUpdate();
+            if (status > 0) {
+                Alert alert = new Alert(Alert.AlertType.NONE);
+                alert.setAlertType(Alert.AlertType.CONFIRMATION);
+                alert.setContentText("Success");
+                alert.show();
+            }
+            else {
+                Alert alert = new Alert(Alert.AlertType.NONE);
+                alert.setAlertType(Alert.AlertType.ERROR);
+                alert.setContentText("Invalid ");
+                alert.show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        
+    
     }   
     public void changeloginButtonPushed(ActionEvent event) throws IOException
     {
--- src/hellofx/Log_in_pageController.java
+++ src/hellofx/Log_in_pageController.java
@@ -6,8 +6,32 @@
 package hellofx;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import hellofx.ConnectionClass;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.sql.*;
+import java.sql.PreparedStatement;
+import java.util.EventObject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  * FXML Controller class
@@ -15,10 +39,61 @@ import javafx.fxml.Initializable;
  * @author Asus
  */
 public class Log_in_pageController implements Initializable {
+    
+        @FXML private TextField email;
+        @FXML private TextField password;
+        ResultSet resultSet = null;
+        PreparedStatement pst = null;
+    
+        public void SignInButton(ActionEvent actionEvent) {
 
-    /**
-     * Initializes the controller class.
-     */
+        String username = email.getText();
+        String pass = password.getText();
+        if ( username.equals("") && pass.equals("")) {
+            Alert a = new Alert(Alert.AlertType.NONE);
+            a.setAlertType(Alert.AlertType.ERROR);
+            a.setContentText("Invalid username or password");
+
+            a.show();
+        } else {
+            try {
+                ConnectionClass connectionClass = new ConnectionClass();
+                Connection connection = connectionClass.getConnection();
+                pst = connection.prepareStatement("select * from user where email=? and password=?");
+                pst.setString(1, username);
+                pst.setString(2, pass);
+                resultSet = pst.executeQuery();
+                if (resultSet.next()) {
+
+
+                    Parent parent = FXMLLoader.load(getClass().getResource("helloFXML.fxml"));
+                    Scene scene = new Scene(parent);
+                    Stage window = (Stage)((Node) actionEvent.getSource()).getScene().getWindow();
+                    window.setScene(scene);
+                    window.show();
+                    window.setMaximized(false);
+
+
+
+
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.NONE);
+                    alert.setAlertType(Alert.AlertType.ERROR);
+                    alert.setContentText("Invalid username or password");
+                    alert.show();
+                }
+
+            } catch (SQLException | IOException ex) {
+                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+
+            }
+        }
+
+
+
+
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
