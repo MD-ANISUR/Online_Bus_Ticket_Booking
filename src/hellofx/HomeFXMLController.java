55121c5f7df26198251770d6b9fea299644d3735
Md. Anisur Rahman <mrahman191191@bscse.uiu.ac.bd>
2022-05-19 16:40:30 +0600

    All work properly but table text doesmt show

--- src/hellofx/HomeFXMLController.java
+++ src/hellofx/HomeFXMLController.java
@@ -6,8 +6,16 @@
 package hellofx;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
@@ -22,29 +30,29 @@ import javafx.scene.control.TableView;
  */
 public class HomeFXMLController implements Initializable {
 
-    @FXML private ComboBox source;
-    @FXML private ComboBox dest;
+    @FXML private ComboBox source1;
+    @FXML private ComboBox dest1;
     @FXML private DatePicker date;
     
     
     @FXML
-    private TableColumn<?, ?> atime;
+    private TableColumn<Service, Integer> atime;
 
     @FXML
-    private TableColumn<?, ?> dtime;
+    private TableColumn<Service, Integer> dtime;
 
     @FXML
-    private TableColumn<?, ?> fare;
+    private TableColumn<Service, Integer> fare;
 
     @FXML
-    private TableColumn<?, ?> name;
+    private TableColumn<Service, String> service;
 
     @FXML
-    private TableView<?> table;
+    private TableView<Service> tableview;
     
     public void getInformation(){
-        String from = (String) source.getValue();
-        String to = (String) dest.getValue();
+        String from = (String) source1.getValue();
+        String to = (String) dest1.getValue();
         LocalDate date1 = date.getValue();
         //System.out.println(from+" "+to+" "+date1);
         
@@ -54,15 +62,57 @@ public class HomeFXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        source.getItems().add("Dhaka");
-        source.getItems().add("Chittagong");
-        source.getItems().add("Rajshahi");
-        source.getItems().add("Cumilla");
-        source.getItems().add("Sylhet");
-        dest.getItems().add("Dhaka");
-        dest.getItems().add("Cumilla");
-        dest.getItems().add("Sylhet");
-        dest.getItems().add("Khulna");
+        source1.getItems().add("Dhaka");
+        source1.getItems().add("Chittagong");
+        source1.getItems().add("Rajshahi");
+        source1.getItems().add("Cumilla");
+        source1.getItems().add("Sylhet");
+        dest1.getItems().add("Dhaka");
+        dest1.getItems().add("Cumilla");
+        dest1.getItems().add("Sylhet");
+        dest1.getItems().add("Khulna");
+        dest1.getItems().add("Rajshahi");
+    }
+
+    public void search(ActionEvent actionEvent)  {
+        loaddata();
+    }
+    private void loaddata() {
+
+
+
+        ObservableList <Service> data = FXCollections.observableArrayList();
+
+
+
+        String source = source1.getSelectionModel().getSelectedItem().toString();
+        String dest = dest1.getSelectionModel().getSelectedItem().toString();
+        String Date = ((LocalDate)this.date.getValue()).format(DateTimeFormatter.ISO_LOCAL_DATE);
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection = connectionClass.getConnection();
+        PreparedStatement pst;
+        try {
+            pst = connection.prepareStatement("select *  from search where source ='"+source+"' and dest = '"+dest+"'and date = '"+Date+"' ");
+            ResultSet rs = pst.executeQuery();
+            while (rs.next())
+            {
+               data.add(new Service(
+                rs.getString(1),
+                rs.getString(2),
+                rs.getString(3),
+                rs.getInt(4),
+                rs.getTime(5),
+                rs.getTime(6),
+                rs.getString(8)));
+
+               tableview.setItems(data);
+                System.out.println(data);
+
+
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
     }    
     
 }
