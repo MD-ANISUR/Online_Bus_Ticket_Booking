35723acd2bbe4e8c114844e862f2ef66216c203b
Md. Anisur Rahman <mrahman191191@bscse.uiu.ac.bd>
2022-05-21 02:42:28 +0600

    AAA

--- src/hellofx/PaymentController.java
+++ src/hellofx/PaymentController.java
@@ -5,9 +5,20 @@
  */
 package hellofx;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,9 +27,32 @@ import javafx.fxml.Initializable;
  */
 public class PaymentController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField amount;
+    @FXML
+    private TextField method;
+    @FXML
+    private Button proceedbtn;
+    @FXML
+    void paymentPressed(ActionEvent event) throws IOException {
+        Alert a = new Alert(Alert.AlertType.NONE);
+        a.setAlertType(Alert.AlertType.CONFIRMATION);
+        a.setContentText("Successfully paid");
+        a.show();
+        
+        
+        Parent parent = FXMLLoader.load(getClass().getResource("homeFXML.fxml"));
+                    Scene scene = new Scene(parent);
+                    Stage window = (Stage)((Node) event.getSource()).getScene().getWindow();
+                    window.setScene(scene);
+                    window.show();
+                    window.setMaximized(false);
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
